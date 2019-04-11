1003c674a918e7e6e231fcff4fa30834da8f6638
温泽清 <13436660226@163.com>
2019-04-11 09:11:01 +0800

    rikao

---
+++ 216.php
@@ -0,0 +1,16 @@
+<?php 
+$data1 = "Have you ever gone shopping and";
+$str = strlen($data1);//31
+$data = array("Have you ever gone shopping and");
+for ($i=0; $i <$str ; $i++) { 
+	$i = $i;
+}
+echo $i;
+
+
+//写出字符串
+//计算长度
+//循环遍历
+//返回
+//字符串出现次数统计
+//上传到linux
