42f3a8e76c010671a2cfd21c31bc6cad23c62ddb
温泽清 <13436660226@163.com>
2019-04-24 08:52:43 +0800

    'la'

--- 219.php
+++ 219.php
@@ -1,26 +1,23 @@
 <?php 
-
 /**
 * 
 */
-class Begin
+class Ha
 {
-	public function ReverseSentence($str){
-		$size="";
-		$new = explode(' ',$str);
-		foreach ($new as $k => $v) {
-			$new = explode(' ',$str);
-			print_r($new);
-			print_r("<br>");
-			$floot = $new['9'];
-			print_r($floot);die;
-			// $size = $v;
-			// print_r($size);
+	static private $n = null;
+	private $arr = null;
+	public function Sum_Solution($n){
+		for ($i=0; $i <=$n ; $i++) { 
+			$i += $i;
 		}
+		$arr = $i-1;
+		return $arr;
 	}
 }
-$str = "q w e r t y u i o p";
-$beg = new Begin();
-$beg->ReverseSentence($str);
+$n = "3";
+$ha = new Ha();
+$res = $ha->Sum_Solution($n);
+print_r($res);
+
 
  ?>
