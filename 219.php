7f42926453a2317986eb364b928fe8e7e9d7e270
温泽清 <13436660226@163.com>
2019-04-26 08:58:49 +0800

    'hah'

--- 219.php
+++ 219.php
@@ -3,28 +3,26 @@
 /**
 * 
 */
-class Test
+class Begin
 {
-	private $num1;
-	private $num2;
-	public function add($num1,$num2){
-		
-		$dbh = new PDO('mysql:dbname=ceshi;host=127.0.0.1', 'root', 'root');
-		$count = $dbh->exec("insert init a values(null,'$num1','$num2')");
-		print_r($count);die;
+	private $wzq = array();
+	private $sum;
+	public function FindNumbersWithSum($wzq,$sum){
+		// print_r($wzq);
+		// echo "<br>";
+		// print_r("$sum");
+		$res = $wzq[0]+$wzq[1];
+		$arr = array($wzq[0],$wzq[1]);
+		return $arr;
 	}
-	public function aadd($num1,$num2){
-		$res = $num1+$num2;
-		
-		print_r($res);
-	}
-	
 }
 
-$num1 = "3";
-$num2 = "4";
-$test = new Test();
-$test->aadd($num1,$num2);
+
+$wzq = ['1','2','3','4','5'];
+$sum = "3";
+$be = new Begin();
+$res = $be->FindNumbersWithSum($wzq,$sum);
+print_r($res);
 
 
 
