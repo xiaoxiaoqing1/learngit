c9dc949d46b4da4616fbd0f411d14c2175477ffd
温泽清 <13436660226@163.com>
2019-04-28 08:49:39 +0800

    '428'

--- 219.php
+++ 219.php
@@ -5,26 +5,20 @@
 */
 class Begin
 {
-	private $wzq = array();
-	private $sum;
-	public function FindNumbersWithSum($wzq,$sum){
-		// print_r($wzq);
-		// echo "<br>";
-		// print_r("$sum");
-		$res = $wzq[0]+$wzq[1];
-		$arr = array($wzq[0],$wzq[1]);
-		return $arr;
+	public function find($target,$array){
+		$arrone = $array['1'];
+		$one = array_count_values($arrone);
+		print_r($one);die;
+		foreach ($array as $k1 => $v1) {
+			print_r($v1);
+
+		}
 	}
 }
 
-
-$wzq = ['1','2','3','4','5'];
-$sum = "3";
-$be = new Begin();
-$res = $be->FindNumbersWithSum($wzq,$sum);
-print_r($res);
-
-
-
+$begin = new Begin();
+$target = 7;
+$array = array('0'=>'1,2,3,4','1'=>'5,6,7,8','2'=>'9,10,11');
+$begin->find($target,$array);
 
  ?>
