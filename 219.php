20ddc59d3da8be251a95367f35b630f0c73beea5
温泽清 <13436660226@163.com>
2019-05-06 08:44:54 +0800

    'first'

--- 219.php
+++ 219.php
@@ -4,29 +4,18 @@
 */
 class Begin
 {
-	public $n = null;
-	public function NumberOf1($n){
-		if ($n<10) {
-			$one_num = $n%10;
-		}else{
-			$one_num = $n%10;
-			$two_num = ($n - $one_num)/10;
-			$res = $two_num.$one_num.$two_num.$one_num;
-			for ($i=0; $i <= 2; $i++) { 
-				$three_num = substr($res,"1");
-			}
-			
-			return 2;
-		}
-
+	public function CountSteps($x,$y){
+		$arr = array($x,$y);
+		$res = count($arr);
+		return $res;
 	}
-	
 }
 
-$n = 10;
+$x = 50;
+$y = 70;
 $begin = new Begin();
-$res = $begin->NumberOf1($n);
-print($res);
+$result = $begin->CountSteps($x,$y);
+print_r($result);
 
 
 
